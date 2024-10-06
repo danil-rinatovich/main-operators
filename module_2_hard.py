fc470bc18a7b58dd4cf5bbca27ed22c204efd546
Danil Rinatovich <kobra-kot@mail.ru>
2024-10-07 01:01:06 +0500

    how delete duplicates?

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,13 +1,13 @@
 import random
 
-random_number = random.randint(3, 20)
-print(random_number)
-list_ = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-password = []
-for i in range(0, len(list_) + 1):
-    for j in range(0, i):
-        if i + j == random_number and i + j % random_number:
-            password.append((i, j))
-            break
-print(password)
+first_field = random.randint(3, 20)
+print(first_field)
+second_field = []
 
+for i in range(1, first_field):
+    for j in range(1, first_field):
+        if i == j:
+            continue
+        elif (i + j) != 0 and first_field % (i + j) == 0:
+            second_field.append([i, j])
+print(second_field)
