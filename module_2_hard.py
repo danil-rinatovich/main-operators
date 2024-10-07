b5afe5d70ee1c6677c0d3fb69e279eec9b8176f0
Danil Ibatullin <danil_rinatovich@list.ru>
2024-10-07 19:00:44 +0500

    final commit

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,10 +1,10 @@
 import random
 
-first_field = random.randint(3, 20)
+first_field = 20#random.randint(3, 20)
 second_field = []
 
 for i in range(1, first_field):
-    for j in range(1, i):
+    for j in range(i + 1, first_field):
         if first_field % (i + j) == 0:
-            second_field.append([j,i])
+            second_field.append([i, j])
 print(f"{first_field} - {second_field}")
